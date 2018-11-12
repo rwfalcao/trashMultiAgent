6d83cde936f702feb145cde1cafae6bc5b541bc0
Rodrigo Wehbe <falcaorw@gmail.com>
2018-11-12 15:55:12 -0200

    implementation of some classes and basic graph plotting

--- classAgent.py
+++ classAgent.py
@@ -1,5 +1,7 @@
 from osbrain import run_agent
 from osbrain import run_nameserver
+import random
+import matplotlib.pyplot as plt
 
 
 def reply(agent, message):
@@ -7,10 +9,33 @@ def reply(agent, message):
     #agent.log_info('Already sent a reply back!') 
 
 class binAgent():
+    agentList = list()
     def __init__(self,alias):
         self.agent = run_agent(alias)
         self.weight = 50
         self.type = 'sensor'
+        self.posX = random.randint(1,30)
+        self.posY = random.randint(1,30)
+        self.agentList.append({'name':alias, 'posX':self.posX, 'posY':self.posY})
+
+class graph():
+    xCoords = list()
+    yCoords = list()
+    def __init__(self, agentList):
+        for agent in agentList:
+            self.xCoords.append(agent['posX'])
+            self.yCoords.append(agent['posY'])
+
+    def plotGraph(self):
+        plt.plot(self.xCoords, self.yCoords, 'ro')
+        plt.plot([15],[15], 'Db')
+        plt.axis([0, 30, 0, 30])
+        plt.show()
+        
+
+       
+   
+            
 
 class mainStationAgent():
     pass
@@ -43,3 +68,5 @@ if __name__ == '__main__':
     
 
     ns.shutdown()
+    gp = graph(binAgent.agentList)
+    gp.plotGraph()
